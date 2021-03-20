d4ecb5856af3befbafb6f9c8a79c4fcdff4c95d0
ifefrost <temiladeladipo@hotmail.com>
2021-03-20 07:29:58 +0100

    End of section 4

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -4,21 +4,41 @@ import EventList from './EventList';
 import { sampleData } from '../../../app/api/sampleData';
 import { useState } from 'react';
 
-function EventDashboard({formOpen, setFormOpen}) {
-    const [events, setEvents] = useState(sampleData);
-    
+export default function EventDashboard({formOpen, setFormOpen, selectEvent, selectedEvent}) {
+  const [events, setEvents] = useState(sampleData);
+  
 
-    return (
-      <Grid>
-        <Grid.Column width={10}>
-          <EventList events={events} />
-        </Grid.Column>
-        <Grid.Column width={6}>
-          {formOpen && 
-          <EventForm setFormOpen={setFormOpen} />}
-        </Grid.Column>
-      </Grid>
-    );
+  function handleCreateEvent(event) {
+    setEvents([...events, event]);
+  }
+
+  function handleUpdateEvent(updatedEvent) {
+    setEvents(events.map(evt => evt.id === updatedEvent.id ? updatedEvent : evt));
+    selectEvent(null); 
+  }
+
+  function handleDeleteEvent(eventId) {
+    setEvents(events.filter(evt => evt.id !== eventId));
+  }
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList events={events} selectEvent={selectEvent} deleteEvent={handleDeleteEvent} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+            selectedEvent={selectedEvent}
+            updateEvent = {handleUpdateEvent}
+            key={selectedEvent ? selectedEvent.id : null}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
 }
 
-export default EventDashboard;
