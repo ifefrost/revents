ab94286d463ee1390a72099efccdd6a63a7e6479
ifefrost <temiladeladipo@hotmail.com>
2021-04-22 11:45:41 +0100

    Update

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,24 +1,40 @@
+import { useState } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
+import SignedInMenu from './SignedInMenu';
+import SignedOutMenu from './SignedOutMenu';
 
-function NavBar({setFormOpen}){
-    return (
-      <Menu inverted fixed='top'>
-        <Container>
-          <Menu.Item header>
-            <img src='/assets/logo.png' alt='logo' style={{marginRight: 15}}/>
-            Re-vents
+function NavBar({ setFormOpen }) {
+  const history = useHistory();
+  const [authenticated, setAuthenticated] = useState(false);
+
+  function handleSignOut() {
+    setAuthenticated(false);
+    history.push('/');
+  }
+
+  return (
+    <Menu inverted fixed='top'>
+      <Container>
+        <Menu.Item as={NavLink} exact to='/' header>
+          <img src='/assets/logo.png' alt='logo' style={{ marginRight: 15 }} />
+          Re-vents
+        </Menu.Item>
+        <Menu.Item as={NavLink} to='/events' name='Events' />
+        <Menu.Item as={NavLink} to='/sandbox' name='Sandbox' />
+        {authenticated && (
+          <Menu.Item as={NavLink} to='/createEvent'>
+            <Button positive inverted content='Create Event' />
           </Menu.Item>
-          <Menu.Item name='Events' />
-          <Menu.Item>
-            <Button onClick={() =>setFormOpen(true)} positive inverted content='Create Event' />
-          </Menu.Item>
-          <Menu.Item position='right'>
-            <Button positive inverted content='Login' />
-            <Button positive inverted content='Register' style={{marginLeft: '0.5em'}} />
-          </Menu.Item>
-        </Container>
-      </Menu>
-    );
+        )}
+        {authenticated ? (
+          <SignedInMenu signOut={handleSignOut} />
+        ) : (
+          <SignedOutMenu setAuthenticated={setAuthenticated} />
+        )}
+      </Container>
+    </Menu>
+  );
 }
 
 export default NavBar;
